799a99b6da3017214a1374cd1581f0b13796fc5e
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-09 15:18:17 +0900

    chore(Inventory): 기본 클래스 및 디렉토리 구조 추가

---
+++ src/main/java/com/chaeum/api/domain/inventory/controller/InventoryController.java
@@ -0,0 +1,4 @@
+package com.chaeum.api.domain.inventory.controller;
+
+public class InventoryController {
+}
---
+++ src/main/java/com/chaeum/api/domain/inventory/service/InventoryService.java
@@ -0,0 +1,4 @@
+package com.chaeum.api.domain.inventory.service;
+
+public class InventoryService {
+}

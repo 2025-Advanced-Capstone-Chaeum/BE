68c376253ce3f0cf364c2f99a82e2b0362dffd2d
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-18 20:14:32 +0900

    feat(Member): 회원 컨트롤러 추가

--- src/main/java/com/chaeum/api/domain/member/controller/MemberController.java
+++ src/main/java/com/chaeum/api/domain/member/controller/MemberController.java
@@ -1,4 +1,16 @@
 package com.chaeum.api.domain.member.controller;
 
+import com.chaeum.api.domain.member.service.MemberService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
+@Tag(name = "Member", description = "회원 관리")
 public class MemberController {
+
+    private final MemberService memberService;
 }

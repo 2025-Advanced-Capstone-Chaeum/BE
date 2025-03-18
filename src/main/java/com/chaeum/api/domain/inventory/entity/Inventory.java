d593f6515890a33ec3359b19ac90c917e5c30b2e
SongJaeHoonn <thdwogns121@naver.com>
2025-03-18 15:07:19 +0900

    feat(Inventory): 인벤토리 엔티티 구현

--- src/main/java/com/chaeum/api/domain/inventory/entity/Inventory.java
+++ src/main/java/com/chaeum/api/domain/inventory/entity/Inventory.java
@@ -1,4 +1,35 @@
 package com.chaeum.api.domain.inventory.entity;
 
-public class Inventory {
+import com.chaeum.api.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "inventory")
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Inventory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private Long itemId;
+
+    @Column(nullable = false)
+    private boolean isWearing;
+
+    @Column(nullable = false)
+    private int quantity;
 }

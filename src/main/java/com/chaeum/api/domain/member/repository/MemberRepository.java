d1bfe36248c63d1cb95433764107aef57442ceac
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-18 20:14:13 +0900

    feat(Member): 회원 레포지토리 추가

--- src/main/java/com/chaeum/api/domain/member/repository/MemberRepository.java
+++ src/main/java/com/chaeum/api/domain/member/repository/MemberRepository.java
@@ -1,4 +1,13 @@
 package com.chaeum.api.domain.member.repository;
 
-public interface MemberRepository {
+import com.chaeum.api.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
 }

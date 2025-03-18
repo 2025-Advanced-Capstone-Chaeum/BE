6cdcc66e79d8d9009cb921610c71a64296d213f2
SongJaeHoonn <thdwogns121@naver.com>
2025-03-18 18:52:55 +0900

    feat(Review): 후기 도메인 추가

---
+++ src/main/java/com/chaeum/api/domain/review/controller/ReviewController.java
@@ -0,0 +1,13 @@
+package com.chaeum.api.domain.review.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/review")
+@RequiredArgsConstructor
+@Tag(name = "Review", description = "알림 관리")
+public class ReviewController {
+}

bbc9b801850cfff509b6668b3d3bb40cf7b0607a
SongJaeHoonn <thdwogns121@naver.com>
2025-03-18 15:37:58 +0900

    feat(InventoryRepository): 인벤토리 dao 구현

--- src/main/java/com/chaeum/api/domain/inventory/repository/InventoryRepository.java
+++ src/main/java/com/chaeum/api/domain/inventory/repository/InventoryRepository.java
@@ -1,4 +1,9 @@
 package com.chaeum.api.domain.inventory.repository;
 
-public interface InventoryRepository {
+import com.chaeum.api.domain.inventory.entity.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 }

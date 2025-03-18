55c7bc6ff1f4d56bc84319c67ddd0752da55d8fd
SongJaeHoonn <thdwogns121@naver.com>
2025-03-18 15:39:33 +0900

    feat(Item): 아이템 엔티티 기본 설정

--- src/main/java/com/chaeum/api/domain/inventory/controller/InventoryController.java
+++ src/main/java/com/chaeum/api/domain/inventory/controller/InventoryController.java
@@ -1,4 +1,16 @@
 package com.chaeum.api.domain.inventory.controller;
 
+import com.chaeum.api.domain.inventory.service.InventoryService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@Tag(name = "Inventory", description = "회원 인벤토리 관리")
 public class InventoryController {
+
+    private final InventoryService inventoryService;
 }

39629f4eb4a32c31ea2a760384561b9e4ec57eab
SongJaeHoonn <thdwogns121@naver.com>
2025-03-18 18:48:40 +0900

    feat(Notification): 알림 도메인 설정

---
+++ src/main/java/com/chaeum/api/domain/notification/service/NotificationService.java
@@ -0,0 +1,9 @@
+package com.chaeum.api.domain.notification.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+}

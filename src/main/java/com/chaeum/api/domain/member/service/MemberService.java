64db65d65a3d53f93045b8187732690919a0a28d
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-18 20:14:24 +0900

    feat(Member): 회원 서비스 추가

--- src/main/java/com/chaeum/api/domain/member/service/MemberService.java
+++ src/main/java/com/chaeum/api/domain/member/service/MemberService.java
@@ -1,4 +1,12 @@
 package com.chaeum.api.domain.member.service;
 
+import com.chaeum.api.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+    private final MemberRepository memberRepository;
+
 }

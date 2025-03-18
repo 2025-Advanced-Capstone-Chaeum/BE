dcc343a5c311912194f11f99c3b1ea8ee6e157b8
SongJaeHoonn <thdwogns121@naver.com>
2025-03-18 15:38:14 +0900

    refactor(InventoryRepository): 인벤토리 엔티티 수정

--- src/main/java/com/chaeum/api/domain/inventory/entity/Inventory.java
+++ src/main/java/com/chaeum/api/domain/inventory/entity/Inventory.java
@@ -1,17 +1,26 @@
 package com.chaeum.api.domain.inventory.entity;
 
+import com.chaeum.api.domain.item.entity.Item;
+import com.chaeum.api.domain.member.entity.Member;
 import com.chaeum.api.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Entity(name = "inventory")
+@Entity
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -21,15 +30,17 @@ public class Inventory extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private Long memberId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
 
     @Column(nullable = false)
-    private Long itemId;
+    private boolean isWearing = false;
 
     @Column(nullable = false)
-    private boolean isWearing;
-
-    @Column(nullable = false)
-    private int quantity;
+    private int quantity = 0;
 }

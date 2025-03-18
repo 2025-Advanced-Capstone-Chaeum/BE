80896aa5de6865f109fb397d9e382dda7519141e
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-18 13:23:11 +0900

    refactor(SwaggerConfig): 서버 설정 방식 어노테이션으로 변경

--- src/main/java/com/chaeum/api/global/config/SwaggerConfig.java
+++ src/main/java/com/chaeum/api/global/config/SwaggerConfig.java
@@ -1,24 +1,25 @@
 package com.chaeum.api.global.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.ArrayList;
-
 @Configuration
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "http://localhost:8080", description = "로컬 서버"),
+                @Server(url = "https://api.example.com", description = "운영 서버")
+        }
+)
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
-        ArrayList<Server> servers = new ArrayList<>();
-        servers.add(new Server().url("https://chaeum.site").description("Chaeum Server"));
-        servers.add(new Server().url("http://localhost:8080").description("Local Server"));
-
         return new OpenAPI()
                 .components(new Components()
                         .addSecuritySchemes("bearer-jwt", new SecurityScheme()
@@ -28,10 +29,8 @@ public class SwaggerConfig {
                                 .bearerFormat("JWT")
                                 .in(SecurityScheme.In.HEADER)
                                 .description("access token을 넣어주세요!"))) // JWT 토큰 추가 시 사용
-                .info(apiInfo())
-                .servers(servers);
-                // 시큐리티 환경 구축 시 추가
-                // .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"));
+                .info(apiInfo());
+        // .addSecurityItem(new SecurityRequirement().addList("bearer-jwt")); // JWT 인증 적용
     }
 
     private Info apiInfo() {

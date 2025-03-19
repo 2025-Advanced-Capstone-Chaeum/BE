ca5ab48b7d81f977d75139181948558399f1cf6a
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-20 00:46:28 +0900

    refactor(Role): EnumSet 제거 및 필드 정리

--- src/main/java/com/chaeum/api/domain/member/entity/Role.java
+++ src/main/java/com/chaeum/api/domain/member/entity/Role.java
@@ -1,51 +1,39 @@
 package com.chaeum.api.domain.member.entity;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import java.util.EnumSet;
 
+@Getter
 @RequiredArgsConstructor
 public enum Role {
 
-    DONOR("ROLE_DONOR", "Donor (기부자)"),
-    RECIPIENT("ROLE_RECIPIENT", "Recipient (수혜자)"),
-    ADMIN("ROLE_ADMIN", "Administrator (관리자)");
+    DONOR("ROLE_DONOR", "Donor (기부자)", 1),
+    RECIPIENT("ROLE_RECIPIENT", "Recipient (수혜자)", 2),
+    ADMIN("ROLE_ADMIN", "Administrator (관리자)", 3);
 
-    private final String key;
-    private final String description;
+    private final String key;         // 역할 키  (ex: ROLE_DONOR)
+    private final String description; // 역할 설명 (ex: 기부자)
+    private final int level;          // 역할 레벨 (숫자가 높을수록 높은 권한)
 
-    public String getKey() {
-        return key;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Long toRoleLevel() {
-        return (long) this.ordinal();
-    }
-
+    // ex. 관리자가 권한 검증 시 사용
     public boolean isHigherThan(Role role) {
-        return this.compareTo(role) > 0;
+        return this.level > role.level;
     }
 
+    // ex. 특정 페이지에 접근할 때 권한 검증 시 사용
     public boolean isHigherThanOrEqual(Role role) {
-        return this.compareTo(role) >= 0;
+        return this.level >= role.level;
     }
-
-    private static final EnumSet<Role> DONOR_ROLES = EnumSet.of(DONOR);
-    private static final EnumSet<Role> RECIPIENT_ROLES = EnumSet.of(RECIPIENT);
-    private static final EnumSet<Role> ADMIN_ROLES = EnumSet.of(ADMIN);
 
     public boolean isDonorRole() {
-        return DONOR_ROLES.contains(this);
+        return this == DONOR;
     }
 
     public boolean isRecipientRole() {
-        return RECIPIENT_ROLES.contains(this);
+        return this == RECIPIENT;
     }
 
     public boolean isAdminRole() {
-        return ADMIN_ROLES.contains(this);
+        return this == ADMIN;
     }
 }

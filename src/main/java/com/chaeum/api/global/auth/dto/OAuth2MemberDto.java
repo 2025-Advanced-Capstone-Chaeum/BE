7ce910306b3a1d385b1ac2f531bf83eca23d79c8
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-19 21:20:48 +0900

    refactor(Auth): OAuth2 인증에 불필요한 Role 필드 삭제

--- src/main/java/com/chaeum/api/global/auth/dto/OAuth2MemberDto.java
+++ src/main/java/com/chaeum/api/global/auth/dto/OAuth2MemberDto.java
@@ -1,14 +1,12 @@
 package com.chaeum.api.global.auth.dto;
 
-import com.chaeum.api.domain.member.entity.Role;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-// OAUTH2 인증 과정에 사용할 유저 객체 (비밀번호는 사용하지 않으므로 제외함)
+// OAUTH2 인증 과정에 사용할 유저 객체
 public class OAuth2MemberDto {
-    private final String name;
     private final String email;
-    private final Role role;
+    private final String name;
 }

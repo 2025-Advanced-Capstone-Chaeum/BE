8533388fb52eb5c1169901193d06de01e3464533
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-20 01:18:04 +0900

    refactor(RefreshToken): 불필요한 띄어쓰기 삭제

--- src/main/java/com/chaeum/api/global/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/chaeum/api/global/auth/repository/RefreshTokenRepository.java
@@ -3,6 +3,6 @@ package com.chaeum.api.global.auth.repository;
 import com.chaeum.api.global.auth.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RefreshTokenRepository  extends CrudRepository<RefreshToken, String> {
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 
 }

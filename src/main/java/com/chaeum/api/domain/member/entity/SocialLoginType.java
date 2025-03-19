494d13f95b70973dbd1c1dcf6b38ff79b7e1e358
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-20 00:48:42 +0900

    refactor(SocialLoginType): key 및 description 필드 추가

--- src/main/java/com/chaeum/api/domain/member/entity/SocialLoginType.java
+++ src/main/java/com/chaeum/api/domain/member/entity/SocialLoginType.java
@@ -1,6 +1,14 @@
 package com.chaeum.api.domain.member.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum SocialLoginType {
-    NAVER,
-    KAKAO
+    NAVER("NAVER", "네이버 로그인"),
+    KAKAO("KAKAO", "카카오 로그인");
+
+    private final String key;          // 로그인 타입 키 (예: NAVER, KAKAO)
+    private final String description;  // 설명 (예: 네이버 로그인, 카카오 로그인)
 }

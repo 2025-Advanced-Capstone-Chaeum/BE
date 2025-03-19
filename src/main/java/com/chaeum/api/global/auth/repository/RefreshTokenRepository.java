d0631c855136023aa03d6e560497af6998afcccd
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-20 01:12:39 +0900

    refactor(RefreshToken): 불필요한 주석 삭제

--- src/main/java/com/chaeum/api/global/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/chaeum/api/global/auth/repository/RefreshTokenRepository.java
@@ -4,5 +4,5 @@ import com.chaeum.api.global.auth.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RefreshTokenRepository  extends CrudRepository<RefreshToken, String> {
-    // JPA Repository가 아닌 Crud Repository를 상속받아야 함
+
 }

127fb9d3c0edffb66b1853a2429fdb73ce9927f9
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-20 00:58:35 +0900

    refactor(Auth): 개행 추가

--- src/main/java/com/chaeum/api/global/auth/dto/OAuth2MemberDto.java
+++ src/main/java/com/chaeum/api/global/auth/dto/OAuth2MemberDto.java
@@ -5,8 +5,8 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-// OAUTH2 인증 과정에 사용할 유저 객체
 public class OAuth2MemberDto {
+
     private final String email;
     private final String name;
 }

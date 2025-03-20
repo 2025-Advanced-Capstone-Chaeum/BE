9011dac141f651a892abb11087f4704e269ea2d5
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-20 23:55:36 +0900

    refactor(RefreshToken): refreshToken 필드 추가 및 갱신 기능 구현

--- src/main/java/com/chaeum/api/global/auth/domain/RefreshToken.java
+++ src/main/java/com/chaeum/api/global/auth/domain/RefreshToken.java
@@ -1,21 +1,28 @@
 package com.chaeum.api.global.auth.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 @Getter
+@NoArgsConstructor
 @RedisHash(value = "refreshToken", timeToLive = 1209600)
 public class RefreshToken {
 
     @Id
     private String memberId;
     private String memberEmail;
-    // redis 저장소의 key 값으로는 {value}:{@Id 어노테이션을 붙여준 값이 됨}
-    // 예를 들어 여기서는 memberId 에 ABC 라는 값을 넣는다면 "memberId":"ABC" 가된다
+    private String refreshToken;
 
-    public RefreshToken(String memberId, String memberEmail) {
+    public RefreshToken(String memberId, String memberEmail, String refreshToken) {
         this.memberId = memberId;
         this.memberEmail = memberEmail;
+        this.refreshToken = refreshToken;
+    }
+
+    // 토큰 갱신
+    public void updateToken(String newToken) {
+        this.refreshToken = newToken;
     }
 }

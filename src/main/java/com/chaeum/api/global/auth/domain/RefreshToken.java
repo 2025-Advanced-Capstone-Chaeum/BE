3b314a7aeaf17135fb4c7e8ba3efb5445a1fa2ee
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-20 21:11:53 +0900

    refactor(RefreshToken): Redis 키 값 refreshToken -> memberId로 변경

--- src/main/java/com/chaeum/api/global/auth/domain/RefreshToken.java
+++ src/main/java/com/chaeum/api/global/auth/domain/RefreshToken.java
@@ -9,13 +9,13 @@ import org.springframework.data.redis.core.RedisHash;
 public class RefreshToken {
 
     @Id
-    private String refreshToken;
+    private String memberId;
     private String memberEmail;
     // redis 저장소의 key 값으로는 {value}:{@Id 어노테이션을 붙여준 값이 됨}
-    // 예를 들어 여기서는 refreshToken 에 ABC 라는 값을 넣는다면 "refreshToken":"ABC" 가된다
+    // 예를 들어 여기서는 memberId 에 ABC 라는 값을 넣는다면 "memberId":"ABC" 가된다
 
-    public RefreshToken(String refreshToken, String memberEmail) {
-        this.refreshToken = refreshToken;
+    public RefreshToken(String memberId, String memberEmail) {
+        this.memberId = memberId;
         this.memberEmail = memberEmail;
     }
 }

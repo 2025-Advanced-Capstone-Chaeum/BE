c507f0d004449ce9314dbecb1d8ec6c2f62927b4
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-21 14:11:37 +0900

    refactor(ExceptionHandler): 로그 포맷 정리 및 로깅 강화

--- src/main/java/com/chaeum/api/global/handler/ChaeumExceptionHandler.java
+++ src/main/java/com/chaeum/api/global/handler/ChaeumExceptionHandler.java
@@ -1,26 +1,37 @@
 package com.chaeum.api.global.handler;
 
 import com.chaeum.api.global.exception.ChaeumException;
-import com.chaeum.api.global.exception.ErrorCode;
 import com.chaeum.api.global.response.ApiResponse;
-import org.springframework.http.ResponseEntity;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class ChaeumExceptionHandler {
+public class ChaeumExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger("ErrorLogger");
+    private static final String LOG_FORMAT_INFO = "\n[🔵INFO] - ({} {})\n(errorCode: {}, className: {})\n{}\n {}: {}";
+    private static final String LOG_FORMAT_WARN = "\n[🟠WARN] - ({} {})\n(id: {}, role: {})";
+    private static final String LOG_FORMAT_ERROR = "\n[🔴ERROR] - ({} {})\n(id: {}, role: {})";
 
     @ExceptionHandler(ChaeumException.class)
-    public ResponseEntity<ApiResponse<Void>> handleChaeumException(ChaeumException ex) {
-        return ResponseEntity
-                .status(ex.getErrorCode().getStatus())
-                .body(ApiResponse.error(ex));
+    public ApiResponse handle(ChaeumException exception, HttpServletRequest request) {
+        logInfo(exception, request);
+        return new ApiResponse<>(exception.getErrorCode());
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse<Void>> handleGeneralException(Exception ex) {
-        return ResponseEntity
-                .status(ErrorCode.INTERNAL_SERVER_ERROR.getStatus())
-                .body(ApiResponse.error(ErrorCode.INTERNAL_SERVER_ERROR));
+    private void logInfo(ChaeumException e, HttpServletRequest request) {
+        log.info(LOG_FORMAT_INFO, request.getMethod(), request.getRequestURI(), e.getErrorCode(), e.getClass().getName(), e.getMessage());
+    }
+
+    private void logWarn(ChaeumException e, HttpServletRequest request) {
+        log.warn(LOG_FORMAT_WARN, request.getMethod(), request.getRequestURI(), e);
+    }
+
+    private void logError(Exception e, HttpServletRequest request) {
+        log.error(LOG_FORMAT_ERROR, request.getMethod(), request.getRequestURI(), e);
     }
 }

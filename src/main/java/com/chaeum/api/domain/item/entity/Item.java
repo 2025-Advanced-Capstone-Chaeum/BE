15e3f21605c6dba5fa6f81730aba9822bd35c8b5
SongJaeHoonn <thdwogns121@naver.com>
2025-03-23 13:51:00 +0900

    feat(Item): Item 엔티티 기본 생성

--- src/main/java/com/chaeum/api/domain/item/entity/Item.java
+++ src/main/java/com/chaeum/api/domain/item/entity/Item.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,6 +19,7 @@ import lombok.Setter;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "item")
 public class Item extends BaseEntity {
 
     @Id

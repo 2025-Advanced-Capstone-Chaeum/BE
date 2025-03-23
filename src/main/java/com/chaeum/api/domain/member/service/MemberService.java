b75c6b8e7767742678bf58907770662d476bf650
SongJaeHoonn <thdwogns121@naver.com>
2025-03-23 18:29:22 +0900

    feat(MemberService): 현재 로그인한 회원 조회 메서드 생성

--- src/main/java/com/chaeum/api/domain/member/service/MemberService.java
+++ src/main/java/com/chaeum/api/domain/member/service/MemberService.java
@@ -1,7 +1,11 @@
 package com.chaeum.api.domain.member.service;
 
+import com.chaeum.api.domain.member.entity.Member;
 import com.chaeum.api.domain.member.repository.MemberRepository;
+import com.chaeum.api.global.auth.dto.CustomMemberDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,4 +14,16 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    public Member getCurrentLoginMember() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return null;
+        }
+        Object principal = authentication.getPrincipal();
+        if (principal instanceof CustomMemberDetails) {
+            return ((CustomMemberDetails) principal).getMember();
+        } else {
+            return null;
+        }
+    }
 }

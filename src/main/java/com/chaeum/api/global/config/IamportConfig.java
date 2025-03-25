5caadf255eaba11b9b44b7b1c7949ec4c803bb06
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-25 14:17:27 +0900

    refactor(IamportConfig): @Value 방식으로 환경 변수 주입 리팩토링

--- src/main/java/com/chaeum/api/global/config/IamportConfig.java
+++ src/main/java/com/chaeum/api/global/config/IamportConfig.java
@@ -1,22 +1,21 @@
 package com.chaeum.api.global.config;
 
-import com.chaeum.api.global.properties.IamportProperties;
 import com.siot.IamportRestClient.IamportClient;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 public class IamportConfig {
 
-    private final IamportProperties iamportProperties;
+    @Value("${spring.portone.api.key}")
+    private String apiKey;
+
+    @Value("${spring.portone.api.secretkey}")
+    private String apiSecret;
 
     @Bean
     public IamportClient iamportClient() {
-        return new IamportClient(
-                iamportProperties.getKey(),
-                iamportProperties.getSecretkey()
-        );
+        return new IamportClient(apiKey, apiSecret);
     }
 }

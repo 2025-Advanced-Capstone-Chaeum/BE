a0daa596e712f97cfd8e1a2e9cef9ef181544f22
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-28 01:46:27 +0900

    refactor(FundingRepository): JpaReposiotry 상속 및 어노테이션 추가

--- src/main/java/com/chaeum/api/domain/funding/repository/FundingRepository.java
+++ src/main/java/com/chaeum/api/domain/funding/repository/FundingRepository.java
@@ -1,4 +1,10 @@
 package com.chaeum.api.domain.funding.repository;
 
-public interface FundingRepository {
+import com.chaeum.api.domain.funding.entity.Funding;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FundingRepository extends JpaRepository<Funding, Long> {
+
 }

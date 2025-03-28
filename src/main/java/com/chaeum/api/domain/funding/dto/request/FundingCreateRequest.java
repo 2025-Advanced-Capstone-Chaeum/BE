8509f1f103038ce41c3e30c535b05f6208304622
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-28 13:33:33 +0900

    refactor(FundingCreateRequest): 펀딩 생성 dto에 불필요한 필드 삭제

--- src/main/java/com/chaeum/api/domain/funding/dto/request/FundingCreateRequest.java
+++ src/main/java/com/chaeum/api/domain/funding/dto/request/FundingCreateRequest.java
@@ -1,8 +1,8 @@
 package com.chaeum.api.domain.funding.dto.request;
 
-import com.chaeum.api.domain.funding.entity.FundingStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,10 +14,12 @@ import java.time.LocalDateTime;
 public class FundingCreateRequest {
 
     @NotNull
+    @Size(max = 100, message = "펀딩 제목은 최대 100자까지 입력할 수 있습니다.")
     @Schema(description = "펀딩 제목", example = "공부해서 사회에 보답하겠습니다.")
     private String title;
 
     @NotNull
+    @Size(max = 1000, message = "펀딩 내용은 최대 1000자까지 입력할 수 있습니다.")
     @Schema(description = "펀딩 내용", example = "학업에 필요한 교재와 책상을 마련하기 위한 펀딩입니다.")
     private String content;
 
@@ -36,14 +38,6 @@ public class FundingCreateRequest {
     @NotNull
     @Schema(description = "목표 금액", example = "1000000.00")
     private BigDecimal goalAmount;
-
-    @NotNull
-    @Schema(description = "현재 모금액", example = "900000.00")
-    private BigDecimal currentAmount;
-
-    @NotNull
-    @Schema(description = "펀딩 상태", example = "ONGOING")
-    private FundingStatus status;
 
     @NotNull
     @Schema(description = "펀딩 종료일", example = "2025-03-28T23:59:59")

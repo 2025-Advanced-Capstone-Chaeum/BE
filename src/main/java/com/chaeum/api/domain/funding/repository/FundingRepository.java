cc015497cc93f82a946e839d0c63b912caf86fb0
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-28 13:36:36 +0900

    refactor(FundingRepository): 상태와 종료일 기준으로 펀딩 조회하는 메서드 추가

--- src/main/java/com/chaeum/api/domain/funding/repository/FundingRepository.java
+++ src/main/java/com/chaeum/api/domain/funding/repository/FundingRepository.java
@@ -1,10 +1,15 @@
 package com.chaeum.api.domain.funding.repository;
 
 import com.chaeum.api.domain.funding.entity.Funding;
+import com.chaeum.api.domain.funding.entity.FundingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface FundingRepository extends JpaRepository<Funding, Long> {
 
+    List<Funding> findByStatusAndEndDateBefore(FundingStatus status, LocalDateTime endDate);
 }

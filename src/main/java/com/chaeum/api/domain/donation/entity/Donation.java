b22cb48f785c40bcc6e4c4812961c343712534ee
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-31 18:31:00 +0900

    feat(Donation): 기부 엔티티 기본 생성

--- src/main/java/com/chaeum/api/domain/donation/entity/Donation.java
+++ src/main/java/com/chaeum/api/domain/donation/entity/Donation.java
@@ -1,4 +1,29 @@
 package com.chaeum.api.domain.donation.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "donation")
 public class Donation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "donation_id")
+    private Long id;
 }

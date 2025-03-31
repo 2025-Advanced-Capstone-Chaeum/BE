156e95207d31ba25df16826f56e8e089c6f44756
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-31 18:27:00 +0900

    feat(Attendance): 출석 엔티티 기본 생성

--- src/main/java/com/chaeum/api/domain/member/entity/Member.java
+++ src/main/java/com/chaeum/api/domain/member/entity/Member.java
@@ -1,6 +1,16 @@
 package com.chaeum.api.domain.member.entity;
 
+import com.chaeum.api.domain.attendance.entity.Attendance;
+import com.chaeum.api.domain.cat.entity.Cat;
+import com.chaeum.api.domain.donation.entity.Donation;
+import com.chaeum.api.domain.friendship.entity.Friendship;
+import com.chaeum.api.domain.inventory.entity.Inventory;
+import com.chaeum.api.domain.missionProgress.entity.MissionProgress;
+import com.chaeum.api.domain.paymentRecord.entity.PaymentRecord;
+import com.chaeum.api.domain.review.entity.Review;
+import com.chaeum.api.domain.title.entity.Title;
 import com.chaeum.api.global.entity.BaseEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -8,6 +18,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -15,6 +27,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -27,48 +41,59 @@ public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
     private Long id;
 
+    @Column(name = "name")
     private String name;
 
-    @Column(nullable = false, unique = true)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(name = "role", nullable = false)
     private Role role;
 
-    @Column(nullable = false)
-    private boolean isBeneficiary = false;
+    @Column(name = "is_beneficiary", nullable = false)
+    private Boolean isBeneficiary = Boolean.FALSE;
 
+    @Column(name = "profile_image")
     private String profileImage;
 
+    @Column(name = "points")
     private int points;
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "social_login_type")
     private SocialLoginType socialLoginType;
 
-//    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
-//    private List<Cat> cats;
-//
-//    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
-//    private List<Inventory> inventoryItems;
-//
-//    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
-//    private List<Title> titles;
-//
-//    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
-//    private List<Attendance> attendanceRecords;
-//
-//    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
-//    private List<MissionProgress> missionProgressList;
-//
-//    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
-//    private List<Donation> donations;
-//
-//    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
-//    private List<Payment> payments;
-//
-//    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
-//    private List<Friendship> friendships;
+    @OneToOne(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Cat cat;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Inventory> inventoryItems;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PaymentRecord> payments;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Donation> donations;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<MissionProgress> missionProgresses;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Friendship> friendships;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Attendance> attendances;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Title> titles;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Notification> notifications;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews;
 }

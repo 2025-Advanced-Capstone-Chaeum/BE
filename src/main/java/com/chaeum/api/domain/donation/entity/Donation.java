90a48fa3ceea6b3b5bbf56afd8ff3438778def25
MinSang22Kim <powerminsang9901@gmail.com>
2025-03-31 22:35:33 +0900

    refactor(Donation): 기부 엔티티 필드 추가

--- src/main/java/com/chaeum/api/domain/donation/entity/Donation.java
+++ src/main/java/com/chaeum/api/domain/donation/entity/Donation.java
@@ -1,10 +1,15 @@
 package com.chaeum.api.domain.donation.entity;
 
+import com.chaeum.api.domain.funding.entity.Funding;
+import com.chaeum.api.domain.member.entity.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -12,6 +17,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Entity
 @Getter
@@ -26,4 +33,18 @@ public class Donation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "donation_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "funding_id", nullable = false)
+    private Funding funding;
+
+    @Column(name = "amount")
+    private BigDecimal amount;
+
+    @Column(name = "status")
+    private DonationStatus status;
 }

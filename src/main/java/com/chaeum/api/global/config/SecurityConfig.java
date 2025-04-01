e3be0b8a3e5f3ee0391bd17a345514bbeb5a124b
SongJaeHoonn <thdwogns121@naver.com>
2025-04-01 15:45:54 +0900

    refactor(SecurityConfig): MethodSecurity 작동

--- src/main/java/com/chaeum/api/global/config/SecurityConfig.java
+++ src/main/java/com/chaeum/api/global/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -28,6 +29,7 @@ import org.springframework.web.cors.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 

534e08ace2203becec8c0015787afb6594df1369
MinSang22Kim <powerminsang9901@gmail.com>
2025-04-02 16:44:13 +0900

    feat(FundingSummaryResponse): 펀딩 내역 요약 조회용 DTO 추가

---
+++ src/main/java/com/chaeum/api/domain/funding/dto/response/FundingSummaryResponse.java
@@ -0,0 +1,29 @@
+package com.chaeum.api.domain.funding.dto.response;
+
+import com.chaeum.api.domain.funding.entity.Funding;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class FundingSummaryResponse {
+
+    private String title;
+
+    private String fundingImage;
+
+    private BigDecimal amount;
+
+    private LocalDateTime createdAt;
+
+    public static FundingSummaryResponse toDto(Funding funding){
+        return FundingSummaryResponse.builder()
+                .title(funding.getTitle())
+                .fundingImage(funding.getFundingImage())
+                .amount(funding.getCurrentAmount())
+                .build();
+    }
+}

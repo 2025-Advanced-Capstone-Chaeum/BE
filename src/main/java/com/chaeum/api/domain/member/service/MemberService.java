e49251892a15e67884eea251b1d5f0ffe94d234a
MinSang22Kim <powerminsang9901@gmail.com>
2025-04-02 16:44:43 +0900

    refactor(MemberService): 회원 서비스 로직 추가

--- src/main/java/com/chaeum/api/domain/member/service/MemberService.java
+++ src/main/java/com/chaeum/api/domain/member/service/MemberService.java
@@ -1,34 +1,91 @@
 package com.chaeum.api.domain.member.service;
 
+import com.chaeum.api.domain.donation.dto.response.DonationSummaryResponse;
+import com.chaeum.api.domain.donation.repository.DonationRepository;
+import com.chaeum.api.domain.donation.service.DonationService;
+import com.chaeum.api.domain.funding.dto.response.FundingSummaryResponse;
+import com.chaeum.api.domain.funding.repository.FundingRepository;
+import com.chaeum.api.domain.member.dto.request.MemberUpdateRequest;
+import com.chaeum.api.domain.member.dto.response.BeneficiaryMyPageResponse;
+import com.chaeum.api.domain.member.dto.response.DonorMyPageResponse;
+import com.chaeum.api.domain.member.dto.response.MemberMyPageResponse;
 import com.chaeum.api.domain.member.entity.Member;
 import com.chaeum.api.domain.member.repository.MemberRepository;
 import com.chaeum.api.global.auth.dto.CustomMemberDetails;
+import com.chaeum.api.global.exception.ChaeumException;
+import com.chaeum.api.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final DonationService donationService;
+    private final DonationRepository donationRepository;
+    private final FundingRepository fundingRepository;
 
     public Member getCurrentLoginMember() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || !authentication.isAuthenticated()) {
-            return null;
+            throw ChaeumException.from(ErrorCode.UNAUTHORIZED);
         }
+
         Object principal = authentication.getPrincipal();
-        if (principal instanceof CustomMemberDetails) {
-            return ((CustomMemberDetails) principal).getMember();
-        } else {
-            return null;
+        if (principal instanceof CustomMemberDetails customMemberDetails) {
+            return customMemberDetails.getMember();
         }
+        throw ChaeumException.from(ErrorCode.UNAUTHORIZED);
     }
 
     public Long getCurrentLoginMemberId() {
-        Member member = getCurrentLoginMember();
-        return member != null ? member.getId() : null;
+        return getCurrentLoginMember().getId();
+    }
+
+    @Transactional(readOnly = true)
+    public MemberMyPageResponse getMemberMyPage(Long memberId) {
+        Member member = findById(memberId);
+        if (member.getIsBeneficiary()) {
+            // 수혜자 마이페이지
+            List<FundingSummaryResponse> fundings = fundingRepository
+                    .findByMemberIdOrderByCreatedAtDesc(memberId).stream()
+                    .map(FundingSummaryResponse::toDto)
+                    .toList();
+            return BeneficiaryMyPageResponse.toDto(member, fundings);
+        } else {
+            // 기부자 마이페이지
+            BigDecimal monthlyAmount = donationService.getThisMonthTotalByMemberId(memberId);
+            BigDecimal yearlyAmount = donationService.getThisYearTotalByMemberId(memberId);
+            List<DonationSummaryResponse> donations = donationRepository
+                    .findByMemberIdOrderByCreatedAtDesc(memberId).stream()
+                    .map(DonationSummaryResponse::toDto)
+                    .toList();
+            return DonorMyPageResponse.toDto(member, monthlyAmount, yearlyAmount, donations);
+        }
+    }
+
+    @Transactional
+    public Long update(Long memberId, MemberUpdateRequest memberUpdateRequest) {
+        Member member = findById(memberId);
+        member.update(memberUpdateRequest);
+        return member.getId();
+    }
+
+    @Transactional
+    public Long delete(Long memberId) {
+        memberRepository.deleteById(memberId);
+        return memberId;
+    }
+
+    private Member findById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> ChaeumException.from(ErrorCode.MEMBER_NOT_FOUND));
     }
 }

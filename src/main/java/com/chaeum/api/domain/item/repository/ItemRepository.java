30c473d3ec6514645048616448e8e5712786e981
MinSang22Kim <powerminsang9901@gmail.com>
2025-04-04 12:53:09 +0900

    refactor(ItemRepository): 카테고리 기반 아이템 조회 로직 추가

--- src/main/java/com/chaeum/api/domain/item/repository/ItemRepository.java
+++ src/main/java/com/chaeum/api/domain/item/repository/ItemRepository.java
@@ -1,9 +1,14 @@
 package com.chaeum.api.domain.item.repository;
 
 import com.chaeum.api.domain.item.entity.Item;
+import com.chaeum.api.domain.item.entity.ItemCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByCategory(ItemCategory category);
 }

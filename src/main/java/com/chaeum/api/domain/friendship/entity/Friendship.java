bf662cfa5a978fe17836b73cfc9e2f4234239626
MinSang22Kim <powerminsang9901@gmail.com>
2025-04-05 23:01:59 +0900

    refactor(Friendship): 친구 엔티티 추가

--- src/main/java/com/chaeum/api/domain/friendship/entity/Friendship.java
+++ src/main/java/com/chaeum/api/domain/friendship/entity/Friendship.java
@@ -1,9 +1,17 @@
 package com.chaeum.api.domain.friendship.entity;
 
+import com.chaeum.api.domain.member.entity.Member;
+import com.chaeum.api.global.entity.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -19,9 +27,42 @@ import lombok.Setter;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Table(name = "friendship")
-public class Friendship {
+public class Friendship extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "friendship_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "friend_id", nullable = false)
+    private Member friend;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private FriendshipStatus status;
+
+    public static Friendship create(Member member, Member friend, FriendshipStatus friendshipStatus) {
+        return Friendship.builder()
+            .member(member)
+            .friend(friend)
+            .status(friendshipStatus)
+            .build();
+    }
+
+    public void accept() {
+        this.status = FriendshipStatus.ACCEPTED;
+    }
+
+    public void reject() {
+        this.status = FriendshipStatus.REJECTED;
+    }
+
+    public void changeStatus(FriendshipStatus status) {
+        this.status = status;
+    }
 }

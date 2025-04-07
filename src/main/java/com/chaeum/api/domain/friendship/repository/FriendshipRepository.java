f88e822610680d314faf1c0f20992b8191222ad3
MinSang22Kim <powerminsang9901@gmail.com>
2025-04-07 23:25:57 +0900

    refactor(FriendshipRepository): 상태별/상태 무관 친구 관계 존재 여부 조회 로직 추가

--- src/main/java/com/chaeum/api/domain/friendship/repository/FriendshipRepository.java
+++ src/main/java/com/chaeum/api/domain/friendship/repository/FriendshipRepository.java
@@ -1,4 +1,15 @@
 package com.chaeum.api.domain.friendship.repository;
 
-public interface FriendshipRepository {
+import com.chaeum.api.domain.friendship.entity.Friendship;
+import com.chaeum.api.domain.friendship.entity.FriendshipStatus;
+import com.chaeum.api.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
+
+    boolean existsByMemberAndFriendAndStatus(Member member, Member friend, FriendshipStatus status);
+
+    boolean existsByMemberAndFriend(Member member, Member friend);
 }

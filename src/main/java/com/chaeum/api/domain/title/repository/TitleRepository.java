b12e981c914c2f99a785a97dfcd885cd984f627f
MinSang22Kim <powerminsang9901@gmail.com>
2025-04-07 22:57:12 +0900

    refactor(TitleService): 활성화된 칭호 조회 로직 추가

--- src/main/java/com/chaeum/api/domain/title/repository/TitleRepository.java
+++ src/main/java/com/chaeum/api/domain/title/repository/TitleRepository.java
@@ -1,4 +1,16 @@
 package com.chaeum.api.domain.title.repository;
 
-public interface TitleRepository {
+import com.chaeum.api.domain.member.entity.Member;
+import com.chaeum.api.domain.title.entity.Title;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TitleRepository extends JpaRepository<Title, Long> {
+
+    List<Title> findByIsActiveTrue();
+
+    Optional<Title> findByMemberAndIsActiveTrue(Member member);
 }

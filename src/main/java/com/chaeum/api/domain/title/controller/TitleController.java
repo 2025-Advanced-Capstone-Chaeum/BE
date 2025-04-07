2ecc04a8e5cc1aada3caf6a94b91e24b19136ace
MinSang22Kim <powerminsang9901@gmail.com>
2025-04-07 23:00:28 +0900

    refactor(TitleController): 칭호 서비스 주입

--- src/main/java/com/chaeum/api/domain/title/controller/TitleController.java
+++ src/main/java/com/chaeum/api/domain/title/controller/TitleController.java
@@ -1,4 +1,16 @@
 package com.chaeum.api.domain.title.controller;
 
+import com.chaeum.api.domain.title.service.TitleService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/title")
+@RequiredArgsConstructor
+@Tag(name = "Title", description = "칭호 관리")
 public class TitleController {
+
+    private final TitleService titleService;
 }

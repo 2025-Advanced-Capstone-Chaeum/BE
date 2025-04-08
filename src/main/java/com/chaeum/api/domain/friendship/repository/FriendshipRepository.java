0a8cfd3149aaa158cc33ebe70db2a432a7294b76
MinSang22Kim <powerminsang9901@gmail.com>
2025-04-08 11:00:44 +0900

    refactor(FriendshipRepository): sender 또는 receiver 기준 특정 친구 상태 조회 쿼리 추가

--- src/main/java/com/chaeum/api/domain/friendship/repository/FriendshipRepository.java
+++ src/main/java/com/chaeum/api/domain/friendship/repository/FriendshipRepository.java
@@ -3,13 +3,16 @@ package com.chaeum.api.domain.friendship.repository;
 import com.chaeum.api.domain.friendship.entity.Friendship;
 import com.chaeum.api.domain.friendship.entity.FriendshipStatus;
 import com.chaeum.api.domain.member.entity.Member;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
 
-    boolean existsByMemberAndFriendAndStatus(Member member, Member friend, FriendshipStatus status);
+    boolean existsBySenderAndReceiverAndStatus(Member sender, Member receiver, FriendshipStatus status);
 
-    boolean existsByMemberAndFriend(Member member, Member friend);
+    boolean existsBySenderAndReceiver(Member sender, Member receiver);
+
+    List<Friendship> findBySenderOrReceiverAndStatus(Member sender, Member receiver, FriendshipStatus status);
 }

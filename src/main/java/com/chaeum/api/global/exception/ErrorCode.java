60f819f43e8ed37f4821dd73ae65338eb4a396f7
SongJaeHoonn <thdwogns121@naver.com>
2025-04-10 20:13:01 +0900

    feat(ErrorCode): 포인트 관련 에러코드 추가

--- src/main/java/com/chaeum/api/global/exception/ErrorCode.java
+++ src/main/java/com/chaeum/api/global/exception/ErrorCode.java
@@ -22,6 +22,7 @@ public enum ErrorCode {
     PAYMENT_VERIFY_FAILED(HttpStatus.BAD_REQUEST, "결제 검증에 실패했습니다."),
     DONATION_AMOUNT_EXCEEDS_GOAL(HttpStatus.BAD_REQUEST, "기부 금액은 목표 금액을 넘을 수 없습니다."),
     INSUFFICIENT_POINTS(HttpStatus.BAD_REQUEST, "포인트가 부족합니다."),
+    INVALID_POINT_AMOUNT(HttpStatus.BAD_REQUEST, "포인트는 음수가 될 수 없습니다."),
     INVALID_FRIENDSHIP_STATUS(HttpStatus.BAD_REQUEST, "존재하지 않거나 잘못된 친구 상태입니다."),
     INVALID_SELF_FRIENDSHIP(HttpStatus.BAD_REQUEST, "자기 자신에게 친구 요청을 보낼 수 없습니다."),
     INVALID_FRIENDSHIP_STATUS_TRANSITION(HttpStatus.BAD_REQUEST, "현재 친구 상태에서는 해당 변경이 불가능합니다."),

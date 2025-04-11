ac0f8d30e5cfc9abd2a1829d4527ce43d2276d0a
MinSang22Kim <powerminsang9901@gmail.com>
2025-04-11 14:49:32 +0900

    refactor(Attendance): 출석 엔티티 필드 및 정적 팩토리 메서드 추가

--- src/main/java/com/chaeum/api/domain/attendance/entity/Attendance.java
+++ src/main/java/com/chaeum/api/domain/attendance/entity/Attendance.java
@@ -1,10 +1,17 @@
 package com.chaeum.api.domain.attendance.entity;
 
+import com.chaeum.api.domain.attendance.dto.request.AttendanceCreateRequest;
+import com.chaeum.api.domain.member.entity.Member;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,5 +30,20 @@ public class Attendance {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "attendance_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(name = "attendance_date")
+    private LocalDate date;
+
+    public static Attendance create(AttendanceCreateRequest attendanceCreateRequest, Member member, LocalDate date) {
+        return Attendance.builder()
+            .member(member)
+            .date(date)
+            .build();
+    }
 }

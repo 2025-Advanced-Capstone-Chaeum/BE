ce8146424584fa1a86e5f134b3d937b42ba91b6f
MinSang22Kim <powerminsang9901@gmail.com>
2025-04-11 16:49:10 +0900

    refactor(AttendanceController): 출석 컨트롤러 description 컨벤션 반영

--- src/main/java/com/chaeum/api/domain/attendance/controller/AttendanceController.java
+++ src/main/java/com/chaeum/api/domain/attendance/controller/AttendanceController.java
@@ -26,7 +26,8 @@ public class AttendanceController {
         description = """
             [모든 Role 가능]<br>
             현재 로그인한 회원과 오늘 날짜를 기준으로 출석을 등록합니다.
-            """)
+            """
+    )
     @PreAuthorize("hasRole('DONOR')")
     @PostMapping("")
     public ApiResponse<Long> save(
@@ -40,12 +41,13 @@ public class AttendanceController {
         description = """
             [모든 Role 가능]<br>
             연도(year)와 월(month)이 파라미터로 전달되면, 해당 월에 로그인한 사용자의 출석 일자를 리스트로 반환합니다.
-            """)
+            """
+    )
     @PreAuthorize("hasRole('DONOR')")
     @GetMapping("")
     public ApiResponse<List<Integer>> getTodayAttendance(
-        @RequestParam int year,
-        @RequestParam int month
+        @RequestParam(name = "year") int year,
+        @RequestParam(name = "month") int month
     ) {
         List<Integer> attendanceDays = attendanceService.getAttendanceDaysByMonth(year, month);
         return ApiResponse.success(attendanceDays);

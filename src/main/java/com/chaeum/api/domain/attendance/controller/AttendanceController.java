266cff56bb842f3c185e5150f096e13e0ecfcf28
MinSang22Kim <powerminsang9901@gmail.com>
2025-04-11 15:59:15 +0900

    refactor(AttendanceController): 출석 api 설명 보강

--- src/main/java/com/chaeum/api/domain/attendance/controller/AttendanceController.java
+++ src/main/java/com/chaeum/api/domain/attendance/controller/AttendanceController.java
@@ -4,13 +4,11 @@ import com.chaeum.api.domain.attendance.service.AttendanceService;
 import com.chaeum.api.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +21,12 @@ public class AttendanceController {
 
     private final AttendanceService attendanceService;
 
-    @Operation(summary = "출석 등록", description = "[모든 Role 가능]")
+    @Operation(
+        summary = "출석 등록",
+        description = """
+            [모든 Role 가능]<br>
+            현재 로그인한 회원과 오늘 날짜를 기준으로 출석을 등록합니다.
+            """)
     @PreAuthorize("hasRole('DONOR')")
     @PostMapping("")
     public ApiResponse<Long> save(
@@ -32,7 +35,12 @@ public class AttendanceController {
         return ApiResponse.success(id);
     }
 
-    @Operation(summary = "출석 일자 조회", description = "[모든 Role 가능]")
+    @Operation(
+        summary = "출석 일자 조회",
+        description = """
+            [모든 Role 가능]<br>
+            연도(year)와 월(month)이 파라미터로 전달되면, 해당 월에 로그인한 사용자의 출석 일자를 리스트로 반환합니다.
+            """)
     @PreAuthorize("hasRole('DONOR')")
     @GetMapping("")
     public ApiResponse<List<Integer>> getTodayAttendance(

26af9b161899b019f0c7a58f947d1fb92387238f
MinSang22Kim <powerminsang9901@gmail.com>
2025-04-13 02:42:02 +0900

    refactor(Member): 현재 회원의 칭호 조회 로직 추가

--- src/main/java/com/chaeum/api/domain/member/repository/MemberRepository.java
+++ src/main/java/com/chaeum/api/domain/member/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package com.chaeum.api.domain.member.repository;
 
 import com.chaeum.api.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +12,7 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email);
+
+    @Query("SELECT m FROM Member m LEFT JOIN FETCH m.titles WHERE m.id = :id")
+    Optional<Member> findByIdWithTitles(@Param("id") Long id);
 }

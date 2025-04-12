b2a45aad467a78e8c3f1bd0ff7a500ebed17b76e
MinSang22Kim <powerminsang9901@gmail.com>
2025-04-13 02:36:39 +0900

    refactor(TitleController): 칭호 컨트롤러 구현

--- src/main/java/com/chaeum/api/domain/title/controller/TitleController.java
+++ src/main/java/com/chaeum/api/domain/title/controller/TitleController.java
@@ -1,8 +1,13 @@
 package com.chaeum.api.domain.title.controller;
 
+import com.chaeum.api.domain.title.dto.response.TitleResponse;
 import com.chaeum.api.domain.title.service.TitleService;
+import com.chaeum.api.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class TitleController {
 
     private final TitleService titleService;
+
+    @Operation(summary = "최근 칭호 조회", description = "[모든 Role 가능]")
+    @PreAuthorize("hasRole('DONOR')")
+    @GetMapping("")
+    public ApiResponse<TitleResponse> getLatestTitle() {
+        return ApiResponse.success(titleService.getTitle());
+    }
 }

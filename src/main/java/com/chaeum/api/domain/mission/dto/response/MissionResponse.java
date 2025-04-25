1c648e89c33d76b3eac5db5dd5c1f20636ada1b0
MinSang22Kim <powerminsang9901@gmail.com>
2025-04-25 12:56:07 +0900

    feat(MissionResponse): 미션 응답 dto 생성

---
+++ src/main/java/com/chaeum/api/domain/mission/dto/response/MissionResponse.java
@@ -0,0 +1,32 @@
+package com.chaeum.api.domain.mission.dto.response;
+
+import com.chaeum.api.domain.mission.entity.Mission;
+import com.chaeum.api.domain.mission.entity.MissionStatus;
+import com.chaeum.api.global.pagination.provider.IdProvider;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MissionResponse implements IdProvider {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String missionImage;
+
+    private MissionStatus status;
+
+    public static MissionResponse toDto(Mission mission) {
+        return MissionResponse.builder()
+            .id(mission.getId())
+            .name(mission.getName())
+            .description(mission.getDescription())
+            .missionImage(mission.getMissionImageUrl())
+            .status(mission.getStatus())
+            .build();
+    }
+}

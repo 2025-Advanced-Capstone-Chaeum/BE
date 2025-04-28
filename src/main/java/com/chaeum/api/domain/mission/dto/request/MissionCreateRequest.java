a6e41758cad864b28f0e3256f472b6395634dcb8
MinSang22Kim <powerminsang9901@gmail.com>
2025-04-28 18:16:59 +0900

    refactor(Mission): 미션 생성 및 수정 dto 필드별 글자 제한 추가

--- src/main/java/com/chaeum/api/domain/mission/dto/request/MissionCreateRequest.java
+++ src/main/java/com/chaeum/api/domain/mission/dto/request/MissionCreateRequest.java
@@ -3,6 +3,7 @@ package com.chaeum.api.domain.mission.dto.request;
 import com.chaeum.api.domain.mission.entity.MissionType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,10 +12,12 @@ import lombok.Setter;
 public class MissionCreateRequest {
 
     @NotNull
+    @Size(min = 1, max = 200)
     @Schema(description = "미션 이름", example = "오늘의 출석 체크")
     private String name;
 
     @NotNull
+    @Size(min = 1, max = 500)
     @Schema(description = "미션 설명", example = "오늘도 출석해서 고양이를 돌봐주세요!")
     private String description;
 

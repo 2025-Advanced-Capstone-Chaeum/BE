123114544fafae0473f4822052c6cc8190dd34af
MinSang22Kim <powerminsang9901@gmail.com>
2025-04-29 20:16:12 +0900

    feat(MemberMission): 랜덤 관련 상수 전역 클래스로 분리

---
+++ src/main/java/com/chaeum/api/global/utils/MemberMissionConstants.java
@@ -0,0 +1,25 @@
+package com.chaeum.api.global.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class MemberMissionConstants {
+
+    public static final int MEMBER_MISSION_ASSIGN_COUNT = 5;
+
+    public static final int ATTENDANCE_PROGRESS_MIN = 1;
+    public static final int ATTENDANCE_PROGRESS_MAX = 1;
+
+    public static final int CAT_PROGRESS_MIN = 1;
+    public static final int CAT_PROGRESS_MAX = 5;
+
+    public static final int DONATION_PROGRESS_MIN = 1;
+    public static final int DONATION_PROGRESS_MAX = 3;
+
+    public static final int FRIEND_PROGRESS_MIN = 1;
+    public static final int FRIEND_PROGRESS_MAX = 3;
+
+    public static final int ITEM_PROGRESS_MIN = 1;
+    public static final int ITEM_PROGRESS_MAX = 1;
+}

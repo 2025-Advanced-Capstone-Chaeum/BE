b0cd26bc649d7339b36c58c946e07c5c11a1b944
MinSang22Kim <powerminsang9901@gmail.com>
2025-04-29 21:49:57 +0900

    refactor(MemberMissionRepository): 회원별 미션 리포지토리 구현

--- src/main/java/com/chaeum/api/domain/memberMission/repository/MemberMissionRepository.java
+++ src/main/java/com/chaeum/api/domain/memberMission/repository/MemberMissionRepository.java
@@ -1,4 +1,12 @@
 package com.chaeum.api.domain.memberMission.repository;
 
-public interface MemberMissionRepository {
+import com.chaeum.api.domain.memberMission.entity.MemberMission;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberMissionRepository extends JpaRepository<MemberMission, Long> {
+
+    List<MemberMission> findByMemberId(Long memberId);
 }

98e3689aefe861ff979ffafdd2722576ac50d7eb
MinSang22Kim <powerminsang9901@gmail.com>
2025-04-30 18:34:03 +0900

    refactor(MockDataInitializer): 스크립트 실행 순서 변경

--- src/main/java/com/chaeum/api/global/config/MockDataInitializer.java
+++ src/main/java/com/chaeum/api/global/config/MockDataInitializer.java
@@ -31,10 +31,11 @@ public class MockDataInitializer {
         "data/08-cat.sql",
         "data/09-attendance.sql",
         "data/10-friendship.sql",
-        "data/11-review.sql",
-        "data/12-title.sql",
-        "data/13-notification.sql",
-        "data/14-payment-record.sql"
+        "data/11-uploaded_file.sql",
+        "data/12-review.sql",
+        "data/13-title.sql",
+        "data/14-notification.sql",
+        "data/15-payment-record.sql"
     };
 
     @PostConstruct

c5baa8c690d0a23e9bbae7c50538aa49304f571a
MinSang22Kim <powerminsang9901@gmail.com>
2025-05-03 17:46:35 +0900

    refactor(donation): 기부 요약 응답 이미지 필드 리팩토링

--- src/main/java/com/chaeum/api/domain/donation/dto/response/DonationSummaryResponse.java
+++ src/main/java/com/chaeum/api/domain/donation/dto/response/DonationSummaryResponse.java
@@ -1,6 +1,9 @@
 package com.chaeum.api.domain.donation.dto.response;
 
 import com.chaeum.api.domain.donation.entity.Donation;
+import com.chaeum.api.global.file.dto.ExternalFileResponse;
+import com.chaeum.api.global.file.entity.UploadedFile;
+import java.util.Optional;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -15,17 +18,25 @@ public class DonationSummaryResponse {
 
     private String title;
 
-    private String imageUrl;
+    private ExternalFileResponse image;
 
     private BigDecimal amount;
 
     private LocalDateTime createdAt;
 
     public static DonationSummaryResponse toDto(Donation donation) {
+        Optional<UploadedFile> firstImage = donation.getFunding().getFundingImages()
+            .stream()
+            .findFirst();
+
+        ExternalFileResponse imageDto = firstImage
+            .map(ExternalFileResponse::toDto)
+            .orElse(null);
+
         return DonationSummaryResponse.builder()
             .id(donation.getId())
             .title(donation.getFunding().getTitle())
-            .imageUrl(donation.getFunding().getFundingImage())
+            .image(imageDto)
             .amount(donation.getAmount())
             .createdAt(donation.getCreatedAt())
             .build();

324ea72922f0f13b9d2fd6f40b7a798c9b584630
MinSang22Kim <powerminsang9901@gmail.com>
2025-05-09 11:32:57 +0900

    refactor(sql): 리뷰 제목 수정

--- src/main/resources/data/11-review.sql
+++ src/main/resources/data/11-review.sql
@@ -4,8 +4,8 @@ TRUNCATE TABLE review RESTART IDENTITY CASCADE;
 
 INSERT INTO review (review_id, funding_id, title, content, created_at, updated_at)
 VALUES
-    (1, 2, '안전장비 정말 필요했는데 감사합니다!',
-     '낙상 걱정이 많았던 어르신이 안전하게 지내실 수 있게 되었어요. 세심한 도움 감사합니다.',
+    (1, 2, '할아버지를 위한 안전장비를 설치하고 싶어요.',
+     '낙상 걱정이 많았던 할아버지가 안전하게 지내실 수 있게 되었어요. 정말 감사합니다.',
      '2025-04-02 10:00:00', '2025-04-02 10:00:00');
 
 -- 시퀀스 재설정

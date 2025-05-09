3988b892c98b7b62bb377e8911adeecc4c1d4401
MinSang22Kim <powerminsang9901@gmail.com>
2025-05-09 13:42:41 +0900

    hotfix(sql): 오타 수정

--- src/main/resources/data/01-member.sql
+++ src/main/resources/data/01-member.sql
@@ -3,7 +3,8 @@
 
 TRUNCATE TABLE member RESTART IDENTITY CASCADE;
 
-INSERT INTO member (member_id, email, name, profile_image, role, social_login_type, is_beneficiary, points, created_at, updated_at)
+INSERT INTO member (member_id, email, name, profile_image, role, social_login_type, is_beneficiary, points, created_at,
+                    updated_at)
 VALUES
 -- 관리자
 (1, 'admin@chaeum.com', '김채움', NULL, 'ADMIN', 'NAVER', FALSE, 0, '2025-02-17 09:00:00', '2025-02-17 09:00:00'),
@@ -13,8 +14,7 @@ VALUES
 (3, 'minji.lee@example.com', '이미진', NULL, 'DONOR', 'KAKAO', FALSE, 26000, '2025-03-03 11:10:00', '2025-03-03 11:10:00'),
 
 -- 수혜자 (4~5)
-(4, 'seoyeon.park@example.com', '박서연', NULL, 'RECIPIENT', 'NAVER', TRUE, 0, '2025-03-11 13:45:00', '2025-03-11 ' ||
-                                                                                                   '13:45:00'),
+(4, 'seoyeon.park@example.com', '박서연', NULL, 'RECIPIENT', 'NAVER', TRUE, 0, '2025-03-11 13:45:00', '2025-03-11 13:45:00'),
 (5, 'jinwoo.kim@example.com', '김진우', NULL, 'RECIPIENT', 'KAKAO', TRUE, 0, '2025-03-12 11:00:00', '2025-03-12 11:00:00');
 
 -- 시퀀스 재설정

a8155f702e163f387a95637545781a14539102be
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-05-16 11:46:10 +0900

    refactor(LoginMemberProvider): 인증 회원 객체 추출 로직 리팩토링

--- src/main/java/com/chaeum/api/global/auth/util/LoginMemberProvider.java
+++ src/main/java/com/chaeum/api/global/auth/util/LoginMemberProvider.java
@@ -4,22 +4,15 @@ import com.chaeum.api.domain.member.entity.Member;
 import com.chaeum.api.global.auth.dto.CustomMemberDetails;
 import com.chaeum.api.global.exception.ChaeumException;
 import com.chaeum.api.global.exception.ErrorCode;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class LoginMemberProvider {
 
     public Member getCurrentLoginMember() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || !authentication.isAuthenticated()) {
-            throw ChaeumException.from(ErrorCode.UNAUTHORIZED);
-        }
-
-        Object principal = authentication.getPrincipal();
+        Object principal = getAuthentication().getPrincipal();
         if (principal instanceof CustomMemberDetails customMemberDetails) {
             return customMemberDetails.member();
         }
@@ -28,5 +21,13 @@ public class LoginMemberProvider {
 
     public Long getCurrentLoginMemberId() {
         return getCurrentLoginMember().getId();
+    }
+
+    private Authentication getAuthentication() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw ChaeumException.from(ErrorCode.UNAUTHORIZED);
+        }
+        return authentication;
     }
 }

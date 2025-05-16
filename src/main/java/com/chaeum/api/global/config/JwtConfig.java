2b0e912a0ca05bb1c0bf4dbaa16c76601431e95d
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-05-16 10:56:54 +0900

    feat(JwtConfig): 시크릿 키 Bean 등록 방식으로 변경

---
+++ src/main/java/com/chaeum/api/global/config/JwtConfig.java
@@ -0,0 +1,21 @@
+package com.chaeum.api.global.config;
+
+import com.chaeum.api.global.properties.JwtProperties;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import javax.crypto.SecretKey;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class JwtConfig {
+
+    private final JwtProperties jwtProperties;
+
+    @Bean
+    public SecretKey jwtSecretKey() {
+        return Keys.hmacShaKeyFor(jwtProperties.getSecretKey().getBytes(StandardCharsets.UTF_8));
+    }
+}

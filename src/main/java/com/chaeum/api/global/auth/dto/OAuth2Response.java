62bf2aa17a7407aaf21422dd6e31541ec925afc2
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-05-16 11:27:59 +0900

    refactor(OAuth2Response): 미사용 필드 삭제

--- src/main/java/com/chaeum/api/global/auth/dto/OAuth2Response.java
+++ src/main/java/com/chaeum/api/global/auth/dto/OAuth2Response.java
@@ -2,9 +2,7 @@ package com.chaeum.api.global.auth.dto;
 
 public interface OAuth2Response {
 
-    String getProvider(); // 제공자 (ex. naver, kakao, ...)
-
-    String getProviderId(); // 제공자에서 발급해주는 번호
+    String getProvider(); // ex. naver, kakao, ...
 
     String getEmail();
 

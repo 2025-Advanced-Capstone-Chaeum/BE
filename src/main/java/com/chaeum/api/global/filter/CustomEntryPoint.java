302081fc673e34a59b77a723ef66bc48696f66b7
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-05-17 15:08:20 +0900

    refactor(CustomEntryPoint): 로깅 삭제

--- src/main/java/com/chaeum/api/global/filter/CustomEntryPoint.java
+++ src/main/java/com/chaeum/api/global/filter/CustomEntryPoint.java
@@ -1,22 +1,15 @@
 package com.chaeum.api.global.filter;
 
 import com.chaeum.api.global.exception.ErrorCode;
-import com.chaeum.api.global.response.ApiResponse;
-import com.chaeum.api.global.response.ErrorResponse;
 import com.chaeum.api.global.utils.ResponseUtil;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@Slf4j
 @Component
 public class CustomEntryPoint implements AuthenticationEntryPoint {
 
@@ -26,7 +19,6 @@ public class CustomEntryPoint implements AuthenticationEntryPoint {
         HttpServletResponse response,
         AuthenticationException e
     ) throws IOException {
-        log.warn("Unauthorized access to {}: {}", request.getRequestURI(), e.getMessage());
         ResponseUtil.writeError(response, ErrorCode.MEMBER_NOT_AUTHENTICATED);
     }
 }
